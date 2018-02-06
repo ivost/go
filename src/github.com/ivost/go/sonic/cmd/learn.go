dded2c2df5c7a96d2bec1f4374cc6c3f816e2f4a
Ivo Stoyanov <ivostoy@gmail.com>
2018-02-06 10:58:36 -0800

    wip

--- src/github.com/ivost/go/sonic/cmd/learn.go
+++ src/github.com/ivost/go/sonic/cmd/learn.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os/exec"
+	"strings"
 
 	// "github.com/golang/dep/gps"
 	"github.com/Masterminds/vcs"
@@ -17,22 +21,28 @@ func runme() {
 	//remote := "https://Ivostoysd@bitbucket.org/smartdrive_systems/gotest2.git"
 
 	remote := "git@bitbucket.org:smartdrive_systems/gotest2.git"
-
 	local, _ := ioutil.TempDir("/tmp", "gotest")
-
 	repo, err := vcs.NewRepo(remote, local)
 	if err != nil {
-		fmt.Printf("ERROR %v\n", err)
+		fmt.Printf("ERROR %q\n", err)
 		return
 	}
-
 	ok := repo.Ping()
-
 	fmt.Printf("REPO %v, PING: %v\n\n", local, ok)
-
 	// ok = repo.CheckLocal()
 	// fmt.Printf("ok %v\n", ok)
 
-	latest, _ := repo.Date()
-	fmt.Printf("latest commit %v\n", latest)
+	fmt.Printf("TEST EXEC\n")
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+
+	// latest, _ := repo.Date()
+	// fmt.Printf("latest commit %v\n", latest)
 }
