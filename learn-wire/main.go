2ef9717ffa5404bfa823063b224fce5ed33429c6
ivost <ivostoy@yahoo.com>
2019-06-23 15:58:52 -0700

    gen

--- learn-wire/main.go
+++ learn-wire/main.go
@@ -1,9 +1,18 @@
 package main
 
+import "log"
+
 func main() {
-	message := NewMessage()
-	greeter := NewGreeter(message)
-	event := NewEvent(greeter)
+
+	//message := NewMessage()
+	//greeter := NewGreeter(message)
+	//event := NewEvent(greeter)
+
+	event, err := InitializeEvent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	event.Start()
 }
 
